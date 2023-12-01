28b4d83b6affc6fd77202c5706bd3c5316062873
deepthipsu <chevuru@pdx.edu>
2023-12-01 13:59:14 -0800

    updates to main module

--- memory_scheduler.sv
+++ memory_scheduler.sv
@@ -12,6 +12,24 @@ begin : initial_blk
 	// Open the file and look for first entry in input file
 	get_first_entry(in_data);
 	
+while (!done)
+begin: while_done
+
+	clock++;
+
+// Check CPU clock and push queue
+	if(req_time_check())	
+		push_queue();
+
+// Check for DIMM clock
+	if(!(clock%2))
+	begin : DIMM_Clk
+// Read first in progress item
+		read_entry_to_process();
+
+	end: DIMM_Clk
+
+end: while_done
         
 end : initial_blk
 endmodule
