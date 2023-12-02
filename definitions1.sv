b2f1e08bfd57f5451862dcad4c77a11ee420040e
Nandini Maddela <nmaddela@ds.cecs.pdx.edu>
2023-12-01 16:33:41 -0800

    new file

--- definitions1.sv
+++ definitions1.sv
@@ -3,6 +3,7 @@ package definitions1;
 
 import declarations0::*;
 import declarations1::*;
+import definitions0::*;
 
 
 // reads the lines of the input file
@@ -16,16 +17,36 @@ function read_from_file(string in_filename);
 endfunction
 
 // check the size of queue
-function size_of_queue();
+function size_queue();
 	if (queue_main.size() < MAX_QUEUE_SIZE) begin
 	  return 1;
+	end else begin 
+	 $display("Queue is full. Cannot enqueue new requestuest.");
 	end
 endfunction
 
 // Pushing into queue
 
 function push_queue();
-     
+	address_mapping(in_data.address, request.mapped_add); 
+	request.cpu_cycles = in_data.operation;
+	request.operation = in_data.operation;
+	 queue_main.push_back(request);
+            $display("[%0d] Pushed requestuest: Time %0d, Core %0p", clock, request);
+            if (debug) begin
+                print_queue_contents(); // Print queue contents after insertion
+            end         
+endfunction
+
+// display contents of queue
+ function print_queue_contents();
+int i;
+        $display("Printing Queue Contents at simulation time %0d", clock);
+        foreach (queue_main[i]) begin
+            $display("Queue Element %0d: Time: %0d, byte_sel: %0d, Address: %h", 
+                     i, queue_main[i].cpu_cycles); //queue_main[i].byte_sel, 
+                     //queue_main[i].address);
+        end
 endfunction
 
 endpackage
